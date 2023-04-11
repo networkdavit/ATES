c9157ed74d3f57dc3d27e404ccb5517aa7f66c53
networkdavit <lonecreation1612@gmail.com>
2023-04-11 23:23:11 +0400

    First admin demo

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,19 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :check_admin, only: [:new, :create, :edit, :update, :destroy]
+
+  def check_admin
+    redirect_to root_path, alert: "You are not authorized to perform this action." unless current_user.admin?
+  end
+
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
 
   def show_by_category
     @category = Category.find(params[:category_id])
@@ -10,5 +22,39 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path(@product)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price,:discount_price, :category_id, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'products#index'
+
   get '/products/category/:category_id', to: 'products#show_by_category', as: 'show_by_category'
-  get 'products/:id', to: 'products#show', as: 'product'
+  get '/products/new', to: 'products#new', as: 'new_product'
+  get '/products/:id', to: 'products#show', as: 'product'
+
+  authenticate :user, ->(user) { user.admin? } do
+    resources :products, only: [:new, :create, :edit, :update, :destroy]
+  end
 end
