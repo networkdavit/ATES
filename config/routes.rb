e27cb415cdc6df48ede541d4853fedb3d89cbb53
networkdavit <lonecreation1612@gmail.com>
2023-04-11 20:50:53 +0400

    Show specific product

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'products#index'
   get '/products/category/:category_id', to: 'products#show_by_category', as: 'show_by_category'
+  get 'products/:id', to: 'products#show', as: 'product'
 end
