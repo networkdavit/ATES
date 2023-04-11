e34f43a64565da7c04afeb43110dda5342d4227e
networkdavit <lonecreation1612@gmail.com>
2023-04-11 14:39:16 +0400

    JS and dropdown

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,9 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def show_by_category
+    @category = Category.find(params[:category_id])
+    @products = @category.products
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'products#index'
+  get '/products/category/:category_id', to: 'products#show_by_category', as: 'show_by_category'
 end
