1d9e9da4c3cf064761f35187a4ad8acd94cede46
networkdavit <lonecreation1612@gmail.com>
2023-04-12 22:44:32 +0400

    Changing routes and allowing password edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  devise_for :user, :path => '', :path_names => { :sign_in => "admin/login", :sign_out => "logout", :sign_up => "admin/new/user/register" }
+
   root 'products#index'
 
   get '/products/category/:category_id', to: 'products#show_by_category', as: 'show_by_category'
